3171fe96cb301899268fc529920d2d9bed97fdb7
Carlos <cracarlos.carlos.etr@gmail.com>
2019-11-12 14:51:21 -0400

    Detalles en el acta

--- app/models/tac_acta.rb
+++ app/models/tac_acta.rb
@@ -9,10 +9,10 @@ class TacActa < ApplicationRecord
   end
 
   def self.generar(generar)
-	select('tac_actas.id,tac_actas.sede,numero_acta, tac_firmante_id,primer_nombre,segundo_nombre,primer_apellido,
-		    segundo_apellido,cedula,cargo,resolucion,competencia').
+	select('tac_actas.id,tac_actas.sede,numero_acta, tac_firmante_id,tac_juramentados.primer_nombre,tac_juramentados.segundo_nombre,tac_juramentados.primer_apellido,
+		    tac_juramentados.segundo_apellido,cedula,tac_juramentados.cargo,resolucion,competencia,nombre_completo,tac_firmantes.cargo').
 	  #joins('INNER JOIN tac_firmantes ON tac_actas.id_firmante = tac_firmantes.id')
-	  joins(:tac_juramentados).
+	  joins(:tac_juramentados, :tac_firmante).
 	  where(id: generar)
   end
 # 'INNER JOIN tac_juramentados ON tac_actas.id = tac_juramentados.tac_acta_id'
