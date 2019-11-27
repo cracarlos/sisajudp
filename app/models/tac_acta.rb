8ae0a773597af40bfc8ce2fcb6ff1e8e8a8ced63
Carlos <cracarlos.carlos.etr@gmail.com>
2019-11-27 15:50:53 -0400

    Acomodando actas

--- app/models/tac_acta.rb
+++ app/models/tac_acta.rb
@@ -17,11 +17,14 @@ class TacActa < ApplicationRecord
   end
 
   def self.generar(generar)
-	select('tac_actas.id,tac_actas.sede,numero_acta, tac_firmante_id,tac_juramentados.primer_nombre,tac_juramentados.segundo_nombre,tac_juramentados.primer_apellido,
-		    tac_juramentados.segundo_apellido,cedula,tac_juramentados.cargo,resolucion,competencia,nombre_completo,tac_firmantes.cargo').
-	  #joins('INNER JOIN tac_firmantes ON tac_actas.id_firmante = tac_firmantes.id')
+	select('tac_actas.id,tac_actas.sede,numero_acta, tac_firmante_id,
+		    tac_juramentados.primer_nombre,tac_juramentados.segundo_nombre,tac_juramentados.primer_apellido,
+		    tac_juramentados.segundo_apellido,cedula,tac_juramentados.cargo AS cargo_j,
+		    tac_juramentados.resolucion,competencia,tac_firmantes.nombre_completo,tac_firmantes.cargo AS cargo_f,
+		    tac_firmantes.nombramiento,
+		    tac_firmantes.titulo'
+		    ).
 	  joins(:tac_juramentados, :tac_firmante).
 	  where(id: generar)
   end
-# 'INNER JOIN tac_juramentados ON tac_actas.id = tac_juramentados.tac_acta_id'
 end
